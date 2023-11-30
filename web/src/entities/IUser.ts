47e1573cd654198f6002329a671a964401102022
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-11-30 00:40:15 -0300

    feat: add  IUser interface for  User

---
+++ web/src/entities/IUser.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+  id: number
+  username: string
+  email: string
+  firstName: string
+  lastName: string
+  gender: string
+  image: string
+  age: number
+  phone: string
+  birthDate: string
+  university: string
+}
