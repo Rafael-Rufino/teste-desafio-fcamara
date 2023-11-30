731ba8379cc20ea0c58f48e9f5a60bfb1360ee44
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-11-30 00:44:06 -0300

    feat: add Routes and Provider to App component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,16 +3,26 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './assets/styles/global'
 import theme from './assets/styles/themes/default'
 
-import Home from './page/Home'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import { Routes } from './Routes'
+import { AuthProvider } from './providers/auth'
+import ToastProvider from './providers/toast'
 
 function App() {
+  // const { user, isAuthenticated } = useAuth()
+  // console.log({ user: user, isAuthenticated: isAuthenticated })
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Home />
-      </ThemeProvider>
-    </>
+    <Router>
+      <AuthProvider>
+        <ToastProvider>
+          <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <Routes />
+          </ThemeProvider>
+        </ToastProvider>
+      </AuthProvider>
+    </Router>
   )
 }
 
