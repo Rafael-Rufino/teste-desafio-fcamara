77337db231a65a8ab5fbf3afaf96991ece12d0df
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-12-01 04:07:57 -0300

    feat: add imports

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -1,5 +1,9 @@
 import { Button } from './button'
+import { FormGroup } from './formGroup'
 import { Header } from './header'
+import { Loader } from './loader'
+import { Logo } from './logo'
+import { Profile } from './profile'
 import { TextInput } from './textInput'
 
-export { Button, Header, TextInput }
+export { Button, FormGroup, Header, Loader, Logo, Profile, TextInput }
