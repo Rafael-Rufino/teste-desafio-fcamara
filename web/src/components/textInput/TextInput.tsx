96798c8df453c23908c560d0b6c09f1ee553518c
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-12-01 19:48:51 -0300

    fix: fix no input problem when typing

--- web/src/components/textInput/TextInput.tsx
+++ web/src/components/textInput/TextInput.tsx
@@ -4,13 +4,12 @@ import * as S from './styles'
 
 type TextInputProps = {
   label?: string
-  name?: string
 } & React.InputHTMLAttributes<HTMLInputElement>
 
-export const TextInput = ({ label, name, ...rest }: TextInputProps) => {
+export const TextInput = ({ label, ...rest }: TextInputProps) => {
   return (
     <S.Container>
-      {label && <S.Label htmlFor={name}>{label}</S.Label>}
+      {label && <S.Label>{label}</S.Label>}
       <S.Input {...rest} />
     </S.Container>
   )
