c442140a6c68b2b42beb4fe49318557f18aedc3f
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-12-01 16:07:57 -0300

    refactor: change tag for label

--- web/src/components/textInput/TextInput.tsx
+++ web/src/components/textInput/TextInput.tsx
@@ -4,12 +4,13 @@ import * as S from './styles'
 
 type TextInputProps = {
   label?: string
+  name: string
 } & React.InputHTMLAttributes<HTMLInputElement>
 
-export const TextInput = ({ label, ...rest }: TextInputProps) => {
+export const TextInput = ({ label, name, ...rest }: TextInputProps) => {
   return (
     <S.Container>
-      {label && <S.Label>{label}</S.Label>}
+      {label && <S.Label htmlFor={name}>{label}</S.Label>}
       <S.Input {...rest} />
     </S.Container>
   )
--- web/src/components/textInput/styles.ts
+++ web/src/components/textInput/styles.ts
@@ -8,7 +8,7 @@ export const Container = styled.div`
   width: 100%;
 `
 
-export const Label = styled.div`
+export const Label = styled.label`
   display: flex;
   padding-bottom: 8px;
 `
