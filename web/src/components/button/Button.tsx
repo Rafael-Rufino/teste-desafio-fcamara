72b09b18b77d2d51b4071079fdd5e379ef677507
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-12-01 04:07:20 -0300

    refactor: refactor Button component and styles

--- web/src/components/button/Button.tsx
+++ web/src/components/button/Button.tsx
@@ -3,15 +3,24 @@ import * as S from './styles'
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'outline' | 'link'
   children?: React.ReactNode
+  color?: string
+  background?: string
 }
 
 export const Button = ({
   variant = 'primary',
+  color,
+  background,
   children,
   ...rest
 }: ButtonProps) => {
   return (
-    <S.Container variant={variant} {...rest}>
+    <S.Container
+      variant={variant}
+      color={color}
+      background={background}
+      {...rest}
+    >
       {children}
     </S.Container>
   )
--- web/src/components/button/styles.ts
+++ web/src/components/button/styles.ts
@@ -4,6 +4,7 @@ import theme from '../../assets/styles/themes/default'
 
 type ButtonProps = {
   variant?: 'primary' | 'link' | 'outline'
+  background?: string
   color?: string
 }
 
@@ -11,34 +12,29 @@ export const Container = styled.button<ButtonProps>`
   display: flex;
   align-items: center;
   justify-content: center;
-  color: ${theme.colors.white};
   width: 100%;
-  border-radius: 4px;
+  border-radius: 0.25rem;
   border: none;
   font-size: ${theme.fonts.sizes.large};
-  gap: 8px;
+  gap: 0.5rem;
 
-  ${({ variant, theme, color }) => {
+  ${({ variant, theme, background, color }) => {
     if (variant === 'primary') {
       return `
         height: 52px;
         box-shadow:  0px 4px 10px rgba(0, 0, 0, 0.04);
-        background-color:  ${
-          color ? theme.colors[color].main : theme.colors.primary.main
-        };
-        color: white;
+        background:  ${background ? [background] : theme.colors.primary.main};
+        color: ${color ? [color] : theme.colors.white};
 
         &:hover {
-          background-color:  ${
-            color ? theme.colors[color].light : theme.colors.primary.light
+          background:  ${
+            background ? [background] : theme.colors.primary.light
           };
           opacity: 0.9;
         }
 
         &:active {
-          background-color:  ${
-            color ? theme.colors[color].dark : theme.colors.primary.dark
-          };
+          background:  ${background ? [background] : theme.colors.primary.dark};
         }
 
         &[disabled]{
@@ -56,9 +52,7 @@ export const Container = styled.button<ButtonProps>`
     if (variant === 'link') {
       return `
       background: transparent;
-        color:  ${
-          color ? theme.colors[color].lighter : theme.colors.primary.main
-        };
+        color:  ${color ? [color] : theme.colors.primary.main};
         &:hover {
           opacity: 0.8;
         }
@@ -69,11 +63,11 @@ export const Container = styled.button<ButtonProps>`
       return `
         height: 52px;
         background: transparent;
-        color: ${theme.colors.primary.main};
-        border: 2px solid ${theme.colors.primary.main};
+        color: ${color ? [color] : theme.colors.primary.main};
+        border: 2px solid ${color ? [color] : theme.colors.primary.main};
         &:hover {
-          background-color: ${theme.colors.primary.light};
-          border-color: ${theme.colors.primary.light};
+          background: ${background ? [background] : theme.colors.primary.light};
+          border-color: ${color ? [color] : theme.colors.primary.light};
         }
       `
     }
