b50684fbcd59dfbf472ecc4547a5003725622c5f
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-12-01 04:09:32 -0300

    refactor: refactor TextInput component

--- web/src/components/textInput/TextInput.tsx
+++ web/src/components/textInput/TextInput.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
+
 import * as S from './styles'
 
-type SearchInputProps = {
+type TextInputProps = {
   label?: string
-  inputRef?: React.Ref<HTMLInputElement>
 } & React.InputHTMLAttributes<HTMLInputElement>
 
-export const TextInput = React.forwardRef<HTMLInputElement, SearchInputProps>(
-  ({ label, ...rest }, ref) => {
-    return (
-      <S.Container>
-        {label && <S.Label>{label}</S.Label>}
-        <S.Input ref={ref} {...rest} />
-      </S.Container>
-    )
-  }
-)
+export const TextInput = ({ label, ...rest }: TextInputProps) => {
+  return (
+    <S.Container>
+      {label && <S.Label>{label}</S.Label>}
+      <S.Input {...rest} />
+    </S.Container>
+  )
+}
