d425a408f8f315d3734345313fbf56bc69e9749e
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-12-01 04:11:39 -0300

    test: add Logo test

---
+++ web/src/components/logo/Logo.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+
+import { Logo } from './Logo'
+
+describe('Logo Component', () => {
+  it('renders correctly', () => {
+    render(<Logo />)
+  })
+})
